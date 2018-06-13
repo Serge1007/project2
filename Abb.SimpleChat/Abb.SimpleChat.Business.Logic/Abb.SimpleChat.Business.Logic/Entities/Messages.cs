c755dd7cfa9fa7865a938328ba5d66a87892644c
Serge1007 <40042989+Serge1007@users.noreply.github.com>
2018-06-13 17:32:37 +0300

    udalil vce

--- Abb.SimpleChat/Abb.SimpleChat.Business.Logic/Abb.SimpleChat.Business.Logic/Entities/Messages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Abb.SimpleChat.Business.Logic.Entities
-{
-    public class Message
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public string Text{ get; set; }
-    }
-}
